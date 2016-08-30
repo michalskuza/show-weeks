d93a370dd7cc122b23d38675c374a8edc2ba45a7
Michal <mmsqzi@gmail.com>
2016-08-30 20:11:09 +0200

    now any day can be used

--- weeknumber-using-format.js
+++ weeknumber-using-format.js
@@ -1,19 +1,18 @@
 var moment = require('moment');
 
-function displayWeeks(start, end, n) {
+function displayWeeks(start, n) {
 
     function format(date){
         return date.format("DD.MM.YYYY")
     }
 
     for (var i = 0; i < n; i++) {
-        console.log('#' + moment(start).add(7 * i, 'days').isoWeek() + ': ' + format(moment(start).add(7 * i, 'days')) + ' - ' + format(moment(end).add(7 * i, 'days')));
+        console.log('#' + moment(start).day(1 + 7 * i).isoWeek() + ': ' + format(moment(start).day(7 * i + 1)) + ' - ' + format(moment(start).day(7 * i + 7)));
     }
 }
 
-var start = new Date(2016, 6, 18)
-var end = new Date(2016, 6, 24)
+var start = new Date(2016, 6, 20)
 
 var n = 5;
 
-displayWeeks(start, end, n);
+displayWeeks(start, n);
