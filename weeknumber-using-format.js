11d9d55a76e8c6f2e81dbfb9ab1c0d8422ce1188
Michal <mmsqzi@gmail.com>
2016-08-30 19:43:56 +0200

    Added weeknumber-using-format.js

---
+++ weeknumber-using-format.js
@@ -0,0 +1,18 @@
+function displayWeeks(start, end, n) {
+    var moment = require('moment');
+
+    function format(date){
+        return date.format("DD.MM.YYYY")
+    }
+
+    for (var i = 0; i < n; i++) {
+        console.log('#' + moment(start).add(7 * i, 'days').isoWeek() + ': ' + format(moment(start).add(7 * i, 'days')) + ' - ' + format(moment(end).add(7 * i, 'days')));
+    }
+}
+
+var start = new Date(2016, 6, 18)
+var end = new Date(2016, 6, 24)
+
+var n = 5;
+
+displayWeeks(start, end, n);
